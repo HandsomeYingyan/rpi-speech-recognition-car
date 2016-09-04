cac4383ce21bc210e28d7513f73b8743fce758aa
HandsomeYingyan <951767826@qq.com>
2016-09-04 13:44:13 +0800

    Some .py to control l298n

---
+++ contror/RIGHT.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+INT1=11
+INT2=12
+INT3=13
+INT4=15
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(INT1,GPIO.OUT)
+GPIO.setup(INT2,GPIO.OUT)
+GPIO.setup(INT3,GPIO.OUT)
+GPIO.setup(INT4,GPIO.OUT)
+GPIO.output(INT1,GPIO.HIGH)
+GPIO.output(INT2,GPIO.LOW)
+GPIO.output(INT3,GPIO.HIGH)
+GPIO.output(INT4,GPIO.LOW)
+time.sleep(0.5)
+GPIO.cleanup()
